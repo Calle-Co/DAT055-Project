127d03eac6ce4ea1e3ac78db76ffe35503de9f53
alwayskebab <78214071+alwayskebab@users.noreply.github.com>
2021-02-24 11:58:04 +0100

    homeView in i mainframe

--- customer/HomeController.java
+++ customer/HomeController.java
@@ -1,5 +1,26 @@
 package customer;
 
+import javax.swing.JButton;
+
+import global.AllButtons;
+
+import java.awt.event.*;
+import java.util.ArrayList;
+
 public class HomeController {
-    
+    private CustomerModel model;
+    private HomeView view;
+    private ArrayList<AllButtons> buttons = new ArrayList<>();
+
+    public HomeController(CustomerModel m, HomeView v){
+        this.model = m;
+        this.view = v;
+        buttons = view.getButtons();
+    }
+
+    public void addButtonListener(ActionListener al) {
+        for(JButton b : buttons) {
+            b.addActionListener(al);
+        }
+    }
 }
