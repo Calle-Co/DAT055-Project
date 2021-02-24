def998281df35d5f2993e12ce8bfa133df17688f
alwayskebab <78214071+alwayskebab@users.noreply.github.com>
2021-02-24 11:38:46 +0100

    home view and controller

---
+++ customer/KEVINTEST.java
@@ -0,0 +1,20 @@
+package customer;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import javax.tools.Tool;
+
+public class KEVINTEST {
+    public static void main(String[] args) {
+        JFrame s = new JFrame();
+        s.setPreferredSize(new Dimension(1200, 800));
+        s.setResizable(false);
+        s.add(new HomeView());
+        s.pack();
+        s.setVisible(true);
+        s.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+}
