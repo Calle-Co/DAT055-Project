021cc2162cc6e2b282dee07eb30f42fefd51a68f
wilhus <72127741+wilhus@users.noreply.github.com>
2021-03-02 17:23:23 +0100

    FlightView finishing touches

--- customer/flight/FlightView.java
+++ customer/flight/FlightView.java
@@ -2,45 +2,48 @@ package customer.flight;
 
 import global.*;
 
-import java.awt.BorderLayout;
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.List;
-
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
 import customer.MenuPanel;
-
 import java.awt.*;
-
 
 /**
  * @author Simon Länsberg, William Husar
- * @version 2021-02-24
+ * @version 2021-03-02
  */
 public class FlightView extends JPanel {
-    private List<FlightInfoButton> myList;
-    private JPanel list;
+    private ArrayList<FlightInfoButton> flightButtons;
     private ArrayList<JButton> buttons;
+    private JPanel list;
 
     public FlightView() {
         buttons = new ArrayList<>();
+        flightButtons = new ArrayList<>();
+        
+        setLayout(null);
+        
         list = new JPanel();
-        list.setLayout(new GridLayout(20,1,10,10));
-        this.setLayout(new BorderLayout());
-        this.setPreferredSize(new Dimension(300,600));
-        myList = new ArrayList<>();
-        this.add(new JScrollPane(list), BorderLayout.CENTER);
+        list.setLayout(new BoxLayout(list, BoxLayout.Y_AXIS));
+        list.setBackground(Color.WHITE);
+        JScrollPane scrollPane = new JScrollPane(list);
+        scrollPane.setBounds(200, 120, 800, 625);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        add(scrollPane);
+        
         MenuPanel menuPanel = new MenuPanel();
-        menuPanel.setPreferredSize(new Dimension(1200,100));
+        menuPanel.setBounds(0, 0, 1200, 100);
         for(AllButtons b : menuPanel.getButtons()) {
             buttons.add(b);
         }
-        this.add(menuPanel, BorderLayout.NORTH);
-        this.setVisible(true);
+        add(menuPanel);
+
+        setVisible(true);
     }
 
     public boolean makeOPane(){
@@ -53,25 +56,28 @@ public class FlightView extends JPanel {
     }
     
     public void addButtonListener(ActionListener al){
-        for(FlightInfoButton f : myList){
+        for(FlightInfoButton f : flightButtons){
             f.addActionListener(al);
         }
     }
 
     public void initButtons(ArrayList<FlightInfoButton> infoButtons) {
-        if(!myList.isEmpty()) {
-            myList.clear();
+        if(!flightButtons.isEmpty()) {
+            flightButtons.clear();
             list.removeAll();
         }
+        list.add(Box.createVerticalStrut(20));
         for(FlightInfoButton fib : infoButtons) {
-            myList.add(fib);
+            flightButtons.add(fib);
+            fib.setMaximumSize(new Dimension(600,100));
+            fib.setAlignmentX(JButton.CENTER_ALIGNMENT);
             list.add(fib);
+            list.add(Box.createVerticalStrut(20));
             buttons.add(fib);
         }
         list.revalidate();
         list.repaint();
     }
-
 
     public ArrayList<JButton> getButtons() { return buttons; }
 }
