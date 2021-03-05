5884e03742585ff4121b4e26f424ae8f243323ab
wilhus <72127741+wilhus@users.noreply.github.com>
2021-03-05 08:06:51 +0100

    Removed yellow, added space for javadoc

--- customer/help/EmptyLabel.java
+++ customer/help/EmptyLabel.java
@@ -4,6 +4,12 @@ import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JTextArea;
 
+/**
+ * 
+ * @author Kevin Hao
+ * @version 2021-03-05
+ */
+@SuppressWarnings("serial")
 public class EmptyLabel extends JTextArea{
     public EmptyLabel(int rows){
         setRows(rows);
