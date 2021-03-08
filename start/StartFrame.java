b08a6251890f5323e63d88e779472ee72a306bf2
wilhus <72127741+wilhus@users.noreply.github.com>
2021-03-08 12:04:40 +0100

    Revert "uh"
    This reverts commit 5b35a4ab70cf4ff3be173c7dbf219a592b32f593.

---
+++ start/StartFrame.java
@@ -0,0 +1,18 @@
+package start;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class StartFrame extends JFrame {
+    public StartFrame() {
+        setPreferredSize(new Dimension(600,600));
+        pack();
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - getHeight()) / 2);
+        setLocation(x,y);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
